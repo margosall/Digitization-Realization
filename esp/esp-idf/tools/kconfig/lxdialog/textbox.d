67db594afa3f827a46226f1f75b6129e03a079f2
Kaido Siimer <kaidosiimer@gmail.com>
2019-10-01 15:23:20 +0300

    gitignore deletion

---
+++ esp/esp-idf/tools/kconfig/lxdialog/textbox.d
@@ -0,0 +1,4 @@
+lxdialog/textbox.o: \
+ /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/textbox.c \
+ /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/dialog.h \
+ /usr/include/ncursesw/ncurses.h
---
+++ esp/esp-idf/tools/kconfig/lxdialog/yesno.d
@@ -0,0 +1,4 @@
+lxdialog/yesno.o: \
+ /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/yesno.c \
+ /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/dialog.h \
+ /usr/include/ncursesw/ncurses.h

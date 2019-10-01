e928ed3de47ef80f150115bf2c24db82cf8c432a
Kaido Siimer <kaidosiimer@gmail.com>
2019-10-01 15:37:46 +0300

    rest in pieces

--- esp/esp-idf/tools/kconfig/conf.d
+++
@@ -1,6 +0,0 @@
-conf.o: \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/conf.c \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lkc.h \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/expr.h \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/list.h \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lkc_proto.h
--- esp/esp-idf/tools/kconfig/lxdialog/checklist.d
+++
@@ -1,4 +0,0 @@
-lxdialog/checklist.o: \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/checklist.c \
- /home/kits/Desktop/Kool/Digitization-Realization/esp/esp-idf/tools/kconfig/lxdialog/dialog.h \
- /usr/include/ncursesw/ncurses.h

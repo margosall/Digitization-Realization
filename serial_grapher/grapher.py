69ef01fe586d18a5718a88b65cbc50f3747abcdd
Kaido Siimer <kaidosiimer@gmail.com>
2019-10-02 21:10:38 +0300

    grapher update

--- serial_grapher/grapher.py
+++ serial_grapher/grapher.py
@@ -4,6 +4,7 @@ from matplotlib import style
 import os
 import numpy as np
 from subprocess import Popen, PIPE
+import time
 
 
 script_path =  os.path.dirname(os.path.realpath(__file__))
@@ -19,33 +20,40 @@ samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
 f_vec = samp_rate*np.arange(chunk/2)/chunk
 
+last_modified = [os.path.getmtime(script_path + '/data.txt')]
+print(last_modified)
 
-def animate(i):
+def animate(i, last_modified):
     try:
-        graph_data = open(script_path + '/data.txt','r+').read()
-        lines = graph_data.split('\n')
-        ys = []
-        for line in lines:
-            if len(line) > 1:
-                ys.append(int(line))
-        ax1.clear()
-        ax2.clear()
-        ax1.set_ylim(-32768, 32767)
-        ax1.plot(ys)
+        new_modified = os.path.getmtime(script_path + '/data.txt')
+        if last_modified[0] != new_modified:
+            print(last_modified, new_modified)
+            last_modified[0] = new_modified
+            with open(script_path + '/data.txt','r+') as graph_data_file:
+                graph_data = graph_data_file.read()
+            lines = graph_data.split('\n')
+            ys = []
+            for line in lines:
+                if len(line) > 1:
+                    ys.append(int(line))
+            ax1.clear()
+            ax2.clear()
+            ax1.set_ylim(-32768, 32767)
+            ax1.plot(ys)
 
-        fft_data = (np.abs(np.fft.fft(ys))[0:int(np.floor(chunk/2))])/chunk
-        fft_data[1:] = 2*fft_data[1:]
-        ax2.set_ylim(0, 32726)
-        plt.xlabel('Frequency [Hz]')
-        # plt.ylabel('Amplitude [Pa]')
-        ax2.set_xscale('log')
-        ax2.plot(f_vec, fft_data, color='red')
+            fft_data = (np.abs(np.fft.fft(ys))[0:int(np.floor(chunk/2))])/chunk
+            fft_data[1:] = 2*fft_data[1:]
+            ax2.set_ylim(0, 32726)
+            plt.xlabel('Frequency [Hz]')
+            # plt.ylabel('Amplitude [Pa]')
+            ax2.set_xscale('log')
+            ax2.plot(f_vec, fft_data, color='red')
     except KeyboardInterrupt:
         process.terminate()
         exit()
     except Exception as e:
         print(e)
 
-ani = animation.FuncAnimation(fig, animate, interval=10)
+ani = animation.FuncAnimation(fig, animate, interval=1000, fargs=(last_modified,))
 plt.show()
 process.terminate()
--- serial_grapher/serial_reader.py
+++ serial_grapher/serial_reader.py
@@ -5,27 +5,22 @@ import matplotlib.pyplot as plt
 
 script_path =  os.path.dirname(os.path.realpath(__file__))
 
-
-with open(script_path + "/data.txt", "w+") as data_file:
-    pass # create empty file
-
 ser = serial.Serial('/dev/ttyUSB0', 921600)
-
-while ser.read() != b'\n':
-    pass
 
 counter = 1
 
 while True:
-    try:
-        reading = ser.readline().decode('utf-8')
+    while ser.in_waiting:
+        try:
+            reading = ser.readline().decode('utf-8')
 
-        values = reading.strip().split(" ")
 
-        values = [value + "\n" for value in values]
+            values = reading.strip().split(" ")
 
-        with open(script_path + "/data.txt", "w+") as data_file:
-            data_file.writelines(values)
+            values = [value + "\n" for value in values]
 
-    except KeyboardInterrupt:
-        ser.close()
+            with open(script_path + "/data.txt", "w+") as data_file:
+                data_file.writelines(values)
+
+        except KeyboardInterrupt:
+            ser.close()
